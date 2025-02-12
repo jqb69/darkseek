8463b49410637472e7a9c033512dd98059f71755
jqb <redpandadog2023@gmail.com>
2025-02-13 02:51:04 +0800

    Update chat_interface.py

--- app/frontend/components/chat_interface.py
+++ app/frontend/components/chat_interface.py
@@ -71,11 +71,11 @@ def chat_interface(session_id):
         st.error("Socket.IO client script not found. Please check the file path.")
 
     # Add Socket.IO event listener
-    st.components.v1.html(
-        '''
-        <script src="https://cdn.socket.io/4.6.0/socket.io.min.js" integrity="sha384-c79GN5VsunZvi+Q/WObgk2in0CbZsHnjEqvFxC5DxHn9lTfNce2WW6h2pH6u/kF+" crossorigin="anonymous"></script>
-        ''',
-        height=0,
-    )
+    #st.components.v1.html(
+     #   '''
+     #   <script src="https://cdn.socket.io/4.6.0/socket.io.min.js" integrity="sha384-c79GN5VsunZvi+Q/WObgk2in0CbZsHnjEqvFxC5DxHn9lTfNce2WW6h2pH6u/kF+" crossorigin="anonymous"></script>
+     #   ''',
+     #   height=0,
+    #)
 
-    handle_chat_input()
+    #handle_chat_input()
